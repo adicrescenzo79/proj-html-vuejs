ad8f5dfbba18c2f3188aff97467ac0bae0ac937d
Alessandro Di Crescenzo <adicrescenzo79@gmail.com>
2021-04-21 16:57:43 +0200

    header nav completo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,22 @@
-// SCOMMENTARE SE SI USA JQUERY
-// $(document).ready(function(){
-//
-// });
-
-// METODO JQUERY PER LA RICHIESTA DI API
-// $.ajax(
-// {
-// url: "https://flynn.boolean.careers/exercises/api/random/boolean",
-// method: "GET",
-// success: function (data, stato) {
-// $("#risultati").html(data);
-// },
-// error: function (richiesta, stato, errori) {
-// alert("E' avvenuto un errore. " + errore);
-// }
-// }
-// );
-
-// SCOMMENTARE SE SI USA VUE
 Vue.config.devtools = true;
 var app = new Vue({
   el: '#root',
   data: {
-    message: 'Hello Vue!'
+    socialIcons: [
+      'facebook-f',
+      'twitter',
+      'instagram',
+      'youtube'
+    ],
+    headerLinks: [
+      'home',
+      'rates',
+      'testimonials',
+      'FAQ',
+      'blog',
+      'contact',
+      'FREE QUOTE'
+    ],
   }
 })
 
